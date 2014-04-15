d8b0e3e0c5c4372a53a5a87389150fb111072a2c
makoto_kw <makoto.kw@gmail.com>
2014-04-15 23:16:49 +0900

    Added grunt watch task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,39 @@
 module.exports = function(grunt) {
+	require('load-grunt-tasks')(grunt);
 	grunt.initConfig({
 		compass: {
-			dist: {
+			prod: {
 				options: {
 					basePath: 'sass',
 					environment: 'production',
 					force: true
 				}
+			},
+			dev: {
+				options: {
+					basePath: 'sass',
+					environment: 'development',
+					force: true,
+					trace: true
+				}
+			}
+		},
+		watch: {
+			sass_dev: {
+				// We watch and compile sass files as normal but don't live reload here
+				files: ['sass/*.scss'],
+				tasks: ['compass:dev']
 			}
 		}
 	});
-	grunt.loadNpmTasks('grunt-contrib-compass');
-	grunt.registerTask('default', ['compass']);
+
+	grunt.registerTask('build', [
+		'compass:prod'
+	]);
+
+	grunt.registerTask('debug', [
+		'watch:sass_dev'
+	]);
+
+	grunt.registerTask('default', ['build']);
 };
